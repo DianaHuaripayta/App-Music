d2f321515fe0c336bd04775f840b7026e2effdf6
Diana Angelica HG <angelicagonzales1610@gmail.com>
2023-10-29 20:51:10 -0500

    demo

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,5 +3,4 @@ import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 const configService = {
     apiUrl: 'https://api.spotify.com/v1/'
 }
-const sdk = SpotifyApi.withClientCredentials("client-id", "secret", ["scope1", "scope2"]);
 export default configService
